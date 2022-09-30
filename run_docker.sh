fd024f86441976eb08a1812f4ec9e9a452d96420
huuhiep2505 <huuhiep2502@github.com>
2022-09-30 15:51:59 +0700

    Update code for run predicate

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,13 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --platform=linux/amd64 --tag=app .
+docker tag app huuhiep2505/project-4-udacity-devops
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 huuhiep2505/project-4-udacity-devops
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=huuhiep2505/project-4-udacity-devops
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deploy udacity --image=$dockerpath:latest
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+pod=$(kubectl get pods --no-headers -o custom-columns=':metadata.name')
+kubectl port-forward pod/$pod 8000:80
